e98d63fa1e8a2a29ddff1079b231606a909f1635
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-10 17:55:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,6 +3,7 @@ sum = num2+num1
 puts sum
 sum
 end
+addition(4,3)
 
 def subtraction (num1, num2)
 sum = num1-num2
@@ -10,3 +11,9 @@ puts sum
 sum
 end
 subtraction(15, 10)
+
+def multiplication (num1, num2)
+  sum = num1*num2
+  puts sum
+end
+multiplication(6,10)
