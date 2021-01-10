041ef83cdc80061eb978e7572b8520ed3b83ffb9
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-10 17:14:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,25 +2,3 @@ def addition (num1, num2)
 num2 + num1 
 end
 addition(2,1)
-
-end
-
-def subtraction(num1, num2)
-
-end
-
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
