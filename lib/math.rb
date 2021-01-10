fb55c63d662281bcf4353c710449b33eef42ccf9
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-10 17:45:53 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,3 +4,29 @@ puts sum
 sum
 end
 addition(2,1)
+
+def subtraction(num1, num2)
+  sum = num2-num1
+  puts sum 
+  sum
+end
+subtration(2,1)
+  
+
+end
+
+def division(num1, num2)
+
+end
+
+def multiplication(num1, num2)
+
+end
+
+def modulo(num1, num2)
+
+end
+
+def square_root(num)
+
+end
