3adc6235719b887eab8e941ed58f5c193b600cf3
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-10 17:53:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,28 +4,11 @@ puts sum
 sum
 end
 
-
-def subtraction(num1, num2)
-sum = num1 - num2
+def subtraction (num1, num2)
+sum = num1-num2
 puts sum
 sum
 end
 subtraction(15, 10)
 
-end
 
-def division(num1, num2)
-
-end
-
-def multiplication(num1, num2)
-
-end
-
-def modulo(num1, num2)
-
-end
-
-def square_root(num)
-
-end
